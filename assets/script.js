8d1dc8401966d153d3836cf9c7af039a66a3cd88
Ben Christensen <ben.eric.christensen@gmail.com>
2022-02-01 10:05:20 -0700

    added second fetch (among other things).

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 var apiKey = '5253d1895ec18b3c6485974e30c75532'
 
 var cityEl = $('#city').val().trim();
-var stateEl = $('#stateEl :selected').val();
+var stateEl = $('#state :selected').val();
 var tempEl = $('#temp')
 var windEl = $('#wind')
 var humidityEl = $('#humidity')
@@ -12,7 +12,7 @@ $('#get-weather-btn').on('click', function() {
 
   function getApi() {
     //uv index seems to only be showing up when the api is called with lat and lon parameters. Will have to make one call with city/state name (as we have below), then pull the lat and lon parameters from that call to make a second call with the newly acquired lat and lon parameters. from there we can grab all the information we need. will also need to do a forecast call. it seems exclude=hourly is a parameter?? possibly will help us avoid the clutter of the three hourly nonsense. 
-  var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityEl},${stateEl},US&units=imperial&appid=${apiKey}`;
+  var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityEl},${stateEl},US&appid=${apiKey}`;
 
     fetch(requestUrl)
       .then(function (response) {
@@ -20,7 +20,18 @@ $('#get-weather-btn').on('click', function() {
       })
       .then(function (data) {
         console.log(data);
+        var lattitude = data.coord.lat;
+        var longitude = data.coord.lon;
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lattitude}&lon=${longitude}&units=imperial&exclude=minutely&appid=${apiKey}`)
+        .then(function (response) {
+          return response.json();
         })
+        //change data variable here --- data is not a built in variable. can be anything.
+        .then(function (data) {
+          console.log(data);
+        })
+        })
+        
 }
 
   getApi();
@@ -33,4 +44,5 @@ $('#get-weather-btn').on('click', function() {
 
 
  
-
+  console.log(stateEl);
+  console.log(cityEl);
