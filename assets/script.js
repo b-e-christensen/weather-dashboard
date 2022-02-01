17b6b7bc974d0385f0fa088fb4c02410574ec50c
rainbowdyno <67548773+rainbowdyno@users.noreply.github.com>
2022-02-01 13:49:44 -0700

    added code to dynamically create card layout for forecast

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,16 @@
 var apiKey = '5253d1895ec18b3c6485974e30c75532'
-
+// variables that capture user input
 var cityEl = $('#city').val().trim();
 var stateEl = $('#state :selected').val();
+// id's that populate the div that displays today's forecast
+var cityAndDateEl = $('#cityAndDate')
 var tempEl = $('#temp')
 var windEl = $('#wind')
 var humidityEl = $('#humidity')
 var uvEl = $('#uvIndex')
 
+var fiveDayForecastEl = $('#fiveDayForecast')
+var forecastArr = [];
 
 $('#get-weather-btn').on('click', function() {
 
@@ -27,14 +31,40 @@ $('#get-weather-btn').on('click', function() {
           return response.json();
         })
         //change data variable here --- data is not a built in variable. can be anything.
-        .then(function (data) {
-          console.log(data);
+        .then(function (oneCallData) {
+          console.log(oneCallData);
+          console.log(oneCallData.current.dt);
+          var dateObject = new Date(oneCallData.current.dt * 1000);
+          var humanDateFormat = dateObject.toLocaleString();
+          console.log(humanDateFormat);
+          cityAndDateEl.text(`${cityEl}, ${stateEl} -- ${humanDateFormat}`);
+          tempEl.text('Temperature: ' + oneCallData.current.temp + '°F');
+          windEl.text('Wind: ' + oneCallData.current.wind_speed + 'mph');
+          humidityEl.text('Humidity: ' + oneCallData.current.humidity + '%');
+          uvEl.text('UV Index: ' + oneCallData.current.uvi);
+
+          for (let i = 0; i < 5; i++) {
+            const element = oneCallData.daily[i];
+            forecastArr.push({
+              dt: element.dt,
+              temp: element.temp.day,
+              wind: element.wind_speed,
+              humidity: element.humidity
+            })                     
+          }
         })
+        // .then(console.log(forecastArr));
         })
+
+// var tempEl = $('#temp')
+// var windEl = $('#wind')
+// var humidityEl = $('#humidity')
+// var uvEl = $('#uvIndex')
         
 }
 
   getApi();
+  console.log(forecastArr);
 
     // var stateVal = $('#state :selected').val()
     // console.log(stateVal);
@@ -46,3 +76,22 @@ $('#get-weather-btn').on('click', function() {
  
   console.log(stateEl);
   console.log(cityEl);
+
+  // var forecastDiv = document.createElement('div');
+  // fiveDayForecastEl.appendChild(forecastDiv);
+  // forecastDiv.addClass('card col-lg-3 col-md-6 col-sm-12')
+  // var temph4 = document.createElement('h4');
+  // var windh4 = document.createElement('h4');
+  // var humidityh4 = document.createElement('h4');
+  // forecastDiv.appendChild(temph4);
+  // forecastDiv.appendChild(windh4);
+  // forecastDiv.appendChild(humidityh4);
+  // temph4.text('Temp: ' + element.temp.day + '°F');
+  // windh4.text('Wind: ' + element.wind_speed + 'mph');
+  // humidityh4.text('Humidity: ' element.humidity + '%')
+//   var forecastDate = new Date (element.dt * 1000)
+//   console.log(forecastDate.toLocaleString("en-US", {
+//     year: "numeric",
+//     month: "2-digit",
+//     day: "numeric"
+// }))
